e0c69dc0942fedb86a5a5a0748437a13dbf17eef
Koon2554 <127096126+Koon2554@users.noreply.github.com>
2025-07-23 20:17:57 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,32 @@
 #include "allinc.hpp"
 using namespace std;
 
-int main() { 
+struct result {
+    string ans;
+    long long time;
+    long long memory;
+};
+
+result calculater(data_num &num, string &line) { 
+    result res;
     start_memory();
+    starttime();
+    if (!num.is_harmoni) {
+        res.ans = longDoubleToString(find_n(num));
+    } else {
+        res.ans = "1/(" + longDoubleToString(find_n(num)) + ")";
+    }
+    res.time = stoptime(); 
+    res.memory = stop_memory();
+    return res;
+}
+
+int main() {
+    result res;
     data_num num;
     string line;
     input(num, line);
-    starttime();
-    Filter_data(num, line);
-    if (!num.is_harmoni) {
-        cout << longDoubleToString(find_n(num));
-    } else {
-        cout << "1/(" << longDoubleToString(find_n(num)) << ")";
-    }
-    cout << "\n" << "-----------------------------";
-    stoptime(); 
-    stop_memory();
+    res = calculater(num, line);
+    cout << res.ans << " " << res.time << " " << res.memory;
     return 0;
 }
