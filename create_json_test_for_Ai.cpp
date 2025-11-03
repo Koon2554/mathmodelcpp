765b66a5d45f14226c3e92a6995bb9aa2b666d6f
Koon2554 <127096126+Koon2554@users.noreply.github.com>
2025-11-03 08:43:12 +0700

    Update create_json_test_for_Ai.cpp

--- create_json_test_for_Ai.cpp
+++ create_json_test_for_Ai.cpp
@@ -1,105 +1,42 @@
 #include <bits/stdc++.h>
 #include "inc/create_test_case.hpp"
 using namespace std;
-#define test_max 1e0
+#define test_max 1e1
 
 int main() {
+    vector<string> seq_names = {"Arithmetic", "Geometric", "Recursive", "Poly_diff", "Poly_div"};
+    random_device rd; mt19937 gen(rd());
+    vector<function<vector<long double>()>> seq_generators = {
+        [](){ return create_arithmetic_sequence(1000000, 1000000, 100); },
+        [](){ return create_geometric_sequence(10, 10, 100); },
+        [](){ return create_recursive_sequence(1000000, 100); },
+        [](){ return create_poly_diff_sequence(1000000, 5, 100); },
+        [](){ return create_poly_div_sequence(9, 4, 100); }
+    };
+    
     ofstream problem("problem.json");
     ofstream answer("answer.json"); 
     string p = "", ans = "";
     p += "[ \n";
     ans += "[ \n";
-    for (int i = 0; i < test_max; i++) {
-        vector<long double> data, num;
-        data = create_arithmetic_sequence(1000000, 1000000, 100);
-        num = vector<long double>(data.begin(), data.begin() + 6);
-        for (int l = 0; l < data.size(); l++) {
+    
+    for (int s = 0; s < seq_names.size(); s++) {
+        for (int i = 0; i < test_max; i++) {
+            vector<long double> data = seq_generators[s](), num;
+            num = vector<long double>(data.begin(), data.begin() + 6);
+            uniform_int_distribution<int> dist_n(0, (int)data.size()-1);
             p += "{ \"id\" : \"";
+            int n = dist_n(gen);
             for (int j = 0; j < num.size(); j++) {
                 p += longDoubleToString(num[j]);
                 if (j != num.size()-1) p += ", ";
                 else {
                     p += "\", \"n\" : ";
-                    p += longDoubleToString(l);
+                    p += longDoubleToString(n);
                     p += " },\n";
                 }
             }
-            ans += "{ \"answer\" : " + longDoubleToString(data[l]) + "}, \n";
-        }
-    }
-    for (int i = 0; i < test_max; i++) {
-        vector<long double> data, num;
-        data = create_geometric_sequence(10, 10, 100);
-        num = vector<long double>(data.begin(), data.begin() + 6);
-        for (int l = 0; l < data.size(); l++) {
-            p += "{ \"id\" : \"";
-            for (int j = 0; j < num.size(); j++) {
-                p += longDoubleToString(num[j]);
-                if (j != num.size()-1) p += ", ";
-                else {
-                    p += "\", \"n\" : ";
-                    p += longDoubleToString(l);
-                    p += " },\n";
-                }
-            }
-            ans += "{ \"answer\" : " + longDoubleToString(data[l]) + "}, \n";
-        }          
-    }
-    for (int i = 0; i < test_max; i++) {
-        vector<long double> data, num;
-        data = create_recursive_sequence(1000000, 100);
-        num = vector<long double>(data.begin(), data.begin() + 6);
-        for (int l = 0; l < data.size(); l++) {
-            p += "{ \"id\" : \"";
-            for (int j = 0; j < num.size(); j++) {
-                p += longDoubleToString(num[j]);
-                if (j != num.size()-1) p += ", ";
-                else {
-                    p += "\", \"n\" : ";
-                    p += longDoubleToString(l);
-                    p += " },\n";
-                }
-            }
-            ans += "{ \"answer\" : " + longDoubleToString(data[l]) + "}, \n";
-        }         
-    }
-
-    for (int i = 0; i < test_max; i++) {
-        vector<long double> data, num;
-        data = create_poly_diff_sequence(1000000, 5, 100);
-        num = vector<long double>(data.begin(), data.begin() + 6);
-        for (int l = 0; l < data.size(); l++) {
-            p += "{ \"id\" : \"";
-            for (int j = 0; j < num.size(); j++) {
-                p += longDoubleToString(num[j]);
-                if (j != num.size()-1) p += ", ";
-                else {
-                    p += "\", \"n\" : ";
-                    p += longDoubleToString(l);
-                    p += " },\n";
-                }
-            }
-            ans += "{ \"answer\" : " + longDoubleToString(data[l]) + "}, \n";
-        }
-    }
-    for (int i = 0; i < test_max; i++) {
-        vector<long double> data, num;
-        data = create_poly_div_sequence(9, 4, 100);
-        num = vector<long double>(data.begin(), data.begin() + 6);
-        for (int l = 0; l < data.size(); l++) {
-            p += "{ \"id\" : \"";
-            for (int j = 0; j < num.size(); j++) {
-                p += longDoubleToString(num[j]);
-                if (j != num.size()-1) p += ", ";
-                else {
-                    p += "\", \"n\" : ";
-                    p += longDoubleToString(l);
-                    if (i == test_max-1 && l == data.size()-1) p += " } \n";
-                    else p += "}, \n";
-                }
-            }
-            if (i == test_max-1 && l == data.size()-1) ans += "{ \"answer\" : " + longDoubleToString(data[l]) + "} \n";
-            else ans += "{ \"answer\" : " + longDoubleToString(data[l]) + "}, \n";
+            ans += "{ \"answer\" : " + longDoubleToString(data[n]) + "}, \n";
         }
     }
     p += "]";
@@ -107,4 +44,5 @@ int main() {
     problem << p;
     answer << ans;
     return 0;
+
 }
