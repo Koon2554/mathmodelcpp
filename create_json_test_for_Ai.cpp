3992c07c61cc7b36d43eac0584ff6bb5dab47966
Koon2554 <127096126+Koon2554@users.noreply.github.com>
2025-12-03 21:00:20 +0700

    Enhance JSON creation with validity check
    Added a check to avoid writing invalid data to JSON.

--- create_json_test_for_Ai.cpp
+++ create_json_test_for_Ai.cpp
@@ -22,23 +22,31 @@ int main() {
     
     for (int s = 0; s < seq_names.size(); s++) {
         for (int i = 0; i < test_max; i++) {
-            vector<long double> data = seq_generators[s](), num;
-            num = vector<long double>(data.begin(), data.begin() + 6);
-            uniform_int_distribution<int> dist_n(0, (int)data.size()-1);
-            p += "    { \"id\" : \"";
-            int n = dist_n(gen);
-            for (int j = 0; j < num.size(); j++) {
-                p += longDoubleToString(num[j]);
-                if (j != num.size()-1) p += ", ";
-                else {
-                    p += "\", \"n\" : ";
-                    p += longDoubleToString(n);
-                    if (s == (int)seq_names.size()-1 && i == test_max-1) p += " }\n";
-                    else p += " },\n";
+            bool c = true;
+            while (c) { 
+                vector<long double> data = seq_generators[s](), num;
+                uniform_int_distribution<int> dist_n(0, (int)data.size()-1);
+                int n = dist_n(gen);
+                if (longDoubleToString(data[n]) != "-9223372036854775808.000") {
+                    c = false;
+                } else {
+                    continue;
                 }
+                num = vector<long double>(data.begin(), data.begin() + 6);
+                p += "    { \"id\" : \"";
+                for (int j = 0; j < num.size(); j++) {
+                    p += longDoubleToString(num[j]);
+                    if (j != num.size()-1) p += ", ";
+                    else {
+                        p += "\", \"n\" : ";
+                        p += longDoubleToString(n);
+                        if (s == (int)seq_names.size()-1 && i == test_max-1) p += " }\n";
+                        else p += " },\n";
+                    }
+                }
+                if (s == (int)seq_names.size()-1 && i == test_max-1) ans += "    { \"answer\" : " + longDoubleToString(data[n]) + "} \n";
+                else ans += "    { \"answer\" : " + longDoubleToString(data[n]) + "}, \n";
             }
-            if (s == (int)seq_names.size()-1 && i == test_max-1) ans += "    { \"answer\" : " + longDoubleToString(data[n]) + "} \n";
-            else ans += "    { \"answer\" : " + longDoubleToString(data[n]) + "}, \n";
         }
     }
     p += "]";
@@ -48,11 +56,3 @@ int main() {
     return 0;
 
 }
-
-
-
-
-
-
-
-
